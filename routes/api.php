a022519f0beb26d27078ad5eedf6a3f032d69c6d
Elivelton Cotrim <elivelton.gbi@gmail.com>
2025-03-26 17:47:03 -0300

    limite de requisições

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::middleware('throttle:10,1')->get('/teste', function(){
+    return response()->json(['message' => 'Hello World!']);
+});
